df09f5d381af7f8575cde2fb9f6bc27f5bdf8045
CruzAvila1212 <724048@student.dusd.net>
2024-05-29 15:27:56 -0700

    Added new builds, and added movement.
    We added the ability to move in different directions and added walls that act as barriers. Also, we added more staircases used to escape.

---
+++ FinalProjectP7/Assets/Polylised - Medieval Desert City/Models/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float turnSpeed = 100.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
